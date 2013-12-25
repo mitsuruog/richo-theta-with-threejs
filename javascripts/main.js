d71ecbe898f77002f7d9329b9c7c63481c68cca6
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2013-12-26 02:46:11 +0900

    adjust indent

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,185 +1,189 @@
-			var camera, scene, renderer, material;
+(function() {
 
-			var fov = 70,
-				texture_placeholder,
-				isUserInteracting = false,
-				onMouseDownMouseX = 0,
-				onMouseDownMouseY = 0,
-				lon = 0,
-				onMouseDownLon = 0,
-				lat = 0,
-				onMouseDownLat = 0,
-				phi = 0,
-				theta = 0;
+	var camera, scene, renderer, material;
 
-			init();
-			animate();
+	var fov = 70,
+		texture_placeholder,
+		isUserInteracting = false,
+		onMouseDownMouseX = 0,
+		onMouseDownMouseY = 0,
+		lon = 0,
+		onMouseDownLon = 0,
+		lat = 0,
+		onMouseDownLat = 0,
+		phi = 0,
+		theta = 0;
 
-			function init() {
+	init();
+	animate();
 
-				var container, mesh;
+	function init() {
 
-				container = document.getElementById('container');
+		var container, mesh;
 
-				camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 1, 1100);
-				camera.target = new THREE.Vector3(0, 0, 0);
+		container = document.getElementById('container');
 
-				scene = new THREE.Scene();
+		camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 1, 1100);
+		camera.target = new THREE.Vector3(0, 0, 0);
 
-				var geometry = new THREE.SphereGeometry(500, 60, 40);
-				geometry.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+		scene = new THREE.Scene();
 
-				material = new THREE.MeshBasicMaterial({
-					map: THREE.ImageUtils.loadTexture('./images/textures/Equirectangular_projection_SW.jpg')
-				});
+		var geometry = new THREE.SphereGeometry(500, 60, 40);
+		geometry.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
 
-				mesh = new THREE.Mesh(geometry, material);
+		material = new THREE.MeshBasicMaterial({
+			map: THREE.ImageUtils.loadTexture('./images/textures/Equirectangular_projection_SW.jpg')
+		});
 
-				scene.add(mesh);
+		mesh = new THREE.Mesh(geometry, material);
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setSize(window.innerWidth, window.innerHeight);
-				container.appendChild(renderer.domElement);
+		scene.add(mesh);
 
-				document.addEventListener('mousedown', onDocumentMouseDown, false);
-				document.addEventListener('mousemove', onDocumentMouseMove, false);
-				document.addEventListener('mouseup', onDocumentMouseUp, false);
-				document.addEventListener('mousewheel', onDocumentMouseWheel, false);
-				document.addEventListener('DOMMouseScroll', onDocumentMouseWheel, false);
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		container.appendChild(renderer.domElement);
 
-				//
+		document.addEventListener('mousedown', onDocumentMouseDown, false);
+		document.addEventListener('mousemove', onDocumentMouseMove, false);
+		document.addEventListener('mouseup', onDocumentMouseUp, false);
+		document.addEventListener('mousewheel', onDocumentMouseWheel, false);
+		document.addEventListener('DOMMouseScroll', onDocumentMouseWheel, false);
 
-				window.addEventListener('resize', onWindowResize, false);
+		//
 
-			}
+		window.addEventListener('resize', onWindowResize, false);
 
-			function onWindowResize() {
+	}
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+	function onWindowResize() {
 
-				renderer.setSize(window.innerWidth, window.innerHeight);
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
 
-			}
+		renderer.setSize(window.innerWidth, window.innerHeight);
 
-			function onDocumentMouseDown(event) {
+	}
 
-				event.preventDefault();
+	function onDocumentMouseDown(event) {
 
-				isUserInteracting = true;
+		event.preventDefault();
 
-				onPointerDownPointerX = event.clientX;
-				onPointerDownPointerY = event.clientY;
+		isUserInteracting = true;
 
-				onPointerDownLon = lon;
-				onPointerDownLat = lat;
+		onPointerDownPointerX = event.clientX;
+		onPointerDownPointerY = event.clientY;
 
-			}
+		onPointerDownLon = lon;
+		onPointerDownLat = lat;
 
-			function onDocumentMouseMove(event) {
+	}
 
-				if (isUserInteracting) {
+	function onDocumentMouseMove(event) {
 
-					lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon;
-					lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
+		if (isUserInteracting) {
 
-				}
-			}
+			lon = (onPointerDownPointerX - event.clientX) * 0.1 + onPointerDownLon;
+			lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
 
-			function onDocumentMouseUp(event) {
+		}
+	}
 
-				isUserInteracting = false;
+	function onDocumentMouseUp(event) {
 
-			}
+		isUserInteracting = false;
 
-			function onDocumentMouseWheel(event) {
+	}
 
-				// WebKit
+	function onDocumentMouseWheel(event) {
 
-				if (event.wheelDeltaY) {
+		// WebKit
 
-					fov -= event.wheelDeltaY * 0.05;
+		if (event.wheelDeltaY) {
 
-					// Opera / Explorer 9
+			fov -= event.wheelDeltaY * 0.05;
 
-				} else if (event.wheelDelta) {
+			// Opera / Explorer 9
 
-					fov -= event.wheelDelta * 0.05;
+		} else if (event.wheelDelta) {
 
-					// Firefox
+			fov -= event.wheelDelta * 0.05;
 
-				} else if (event.detail) {
+			// Firefox
 
-					fov += event.detail * 1.0;
+		} else if (event.detail) {
 
-				}
+			fov += event.detail * 1.0;
 
-				camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
-				render();
+		}
 
-			}
+		camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
+		render();
 
-			function animate() {
+	}
 
-				requestAnimationFrame(animate);
-				render();
+	function animate() {
 
-			}
+		requestAnimationFrame(animate);
+		render();
 
-			function render() {
+	}
 
-				lat = Math.max(-85, Math.min(85, lat));
-				phi = THREE.Math.degToRad(90 - lat);
-				theta = THREE.Math.degToRad(lon);
+	function render() {
 
-				camera.target.x = 500 * Math.sin(phi) * Math.cos(theta);
-				camera.target.y = 500 * Math.cos(phi);
-				camera.target.z = 500 * Math.sin(phi) * Math.sin(theta);
+		lat = Math.max(-85, Math.min(85, lat));
+		phi = THREE.Math.degToRad(90 - lat);
+		theta = THREE.Math.degToRad(lon);
 
-				camera.lookAt(camera.target);
+		camera.target.x = 500 * Math.sin(phi) * Math.cos(theta);
+		camera.target.y = 500 * Math.cos(phi);
+		camera.target.z = 500 * Math.sin(phi) * Math.sin(theta);
 
-				/*
+		camera.lookAt(camera.target);
+
+		/*
 				// distortion
 				camera.position.x = - camera.target.x;
 				camera.position.y = - camera.target.y;
 				camera.position.z = - camera.target.z;
 				*/
 
-				renderer.render(scene, camera);
+		renderer.render(scene, camera);
 
-			}
+	}
 
-			/**
-			 *画像Drag＆Drop処理
-			 */
+	/**
+	 *画像Drag＆Drop処理
+	 */
 
-			function cancelEvent(e) {
-				e.preventDefault();
-				e.stopPropagation();
-			}
+	function cancelEvent(e) {
+		e.preventDefault();
+		e.stopPropagation();
+	}
 
-			function handllerDroppedFile(e) {
-				//単一ファイルの想定
-				var file = e.dataTransfer.files[0];
+	function handllerDroppedFile(e) {
+		//単一ファイルの想定
+		var file = e.dataTransfer.files[0];
 
-				if (!file.type.match('image.*')) {
-					alert('imageファイルにしてね');
-					cancelEvent(e);
-				}
+		if (!file.type.match('image.*')) {
+			alert('imageファイルにしてね');
+			cancelEvent(e);
+		}
 
-				var img = document.createElement("img");
-				var fileReader = new FileReader();
-				fileReader.onload = function(e) {
-					img.src = e.target.result;
-					material.map = new THREE.Texture(img);
-					material.map.needsUpdate = true;
-				};
-				fileReader.readAsDataURL(file);
-				//デフォルトのイベントキャンセルしないとブラウザでイメージが表示されてしまう
-				cancelEvent(e);
-			}
+		var img = document.createElement("img");
+		var fileReader = new FileReader();
+		fileReader.onload = function(e) {
+			img.src = e.target.result;
+			material.map = new THREE.Texture(img);
+			material.map.needsUpdate = true;
+		};
+		fileReader.readAsDataURL(file);
+		//デフォルトのイベントキャンセルしないとブラウザでイメージが表示されてしまう
+		cancelEvent(e);
+	}
 
-			var droppable = document.getElementById('container');
-			droppable.addEventListener('dradenter', cancelEvent);
-			droppable.addEventListener('dragover', cancelEvent);
-			droppable.addEventListener('drop', handllerDroppedFile);
+	var droppable = document.getElementById('container');
+	droppable.addEventListener('dradenter', cancelEvent);
+	droppable.addEventListener('dragover', cancelEvent);
+	droppable.addEventListener('drop', handllerDroppedFile);
+	
+})();
